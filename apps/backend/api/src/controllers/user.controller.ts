59d6e0e165fb81d56a632a129601e67fdb2bca2c
rabra <rabira.hierpa@gmail.com>
2023-11-16 21:24:00 +0300

    🚚  feat: adds auth and user controllers

---
+++ apps/backend/api/src/controllers/user.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { IUser } from '../models/User/user-schema';
+import convertAccess from '../utils/helpers/convertAcces';
+
+/**
+ * Retrieves the user profile information and sends it as a response.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @returns A Promise that resolves to void
+ */
+export async function userProfile(req: Request, res: Response): Promise<void> {
+  const user = req.user as IUser;
+  //get id from req.sessionID
+  res.status(200).send({
+    verified: true,
+    message: 'welcome to your profile',
+    user: user,
+    session: req.sessionID,
+    access: await convertAccess(user.userAccess),
+  });
+}
+
+export async function LoggedInUserProfile(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  const user: any = req.user;
+  const { salt, hash, ...userInfo } = user._doc;
+  console.log({ userInfo });
+  res.status(200).send({
+    user: {
+      ...userInfo,
+      session: req.sessionID,
+      access: await convertAccess(userInfo.userAccess),
+    },
+  });
+}
